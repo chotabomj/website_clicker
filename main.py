ad3a274c233fc92ab44e4c71f130464bc83f1196
chotab <chotabomj@gmail.com>
2021-10-14 15:27:32 +0200

    code refactoring

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def tuple_match (A, B):
         return B == tuple(a for a in A if a in B)
 
 
-def get_header_indexes(sheet, keys_tuple: tuple, start_row=1, start_col=1):
+def get_row_indexes(sheet, keys_tuple: tuple, start_row=1, start_col=1):
     indexes = {}     
     current_row_index = 1                           
     for row in sheet.iter_rows(min_row=start_row, max_row=sheet.max_row, min_col=start_col, max_col=sheet.max_column, values_only=True):
@@ -22,47 +22,34 @@ def get_header_indexes(sheet, keys_tuple: tuple, start_row=1, start_col=1):
         if tuple_match(keys_tuple, row):
             for key in keys_tuple:
                 indexes[key] = row.index(key)
-            return indexes, current_row_index
+            return current_row_index, indexes
         current_row_index += 1
 
 
-def create_users_json(sheet, user_indexes, header_row):
+def create_users_json(sheet, header):
     users = {}
-    start_row = header_row + 1           # первая строчка после шапки
-    user_index = user_indexes['login_index']
-    user_password = user_indexes['password_index']
-    user_sex = user_indexes['sex_index']
-    user_age = user_indexes['age_index']
+    user = {}
     user_id = 0
+    header_row, user_indexes = get_row_indexes(sheet, header) 
+    start_row = header_row + 1           # первая строчка после шапки    
     for row in sheet.iter_rows(min_row=start_row, values_only=True):
-        user = {
-            'completed_test': False,
-            'login': row[user_index],
-            'password': row[user_password],
-            'sex': row[user_sex],
-            'age': row[user_age]
-        }
-        users[user_id] = user
-        user_id += 1
-    return json.dumps(users, indent=4, ensure_ascii=False) # ensure_ascii=False подключает utf-8
+        row = get_tuple_to_lowercase(row)
+        for column_name in header:
+            user[column_name] = row[user_indexes[column_name]]
+        users[user_id] = user    
+        user_id += 1 
+    return json.dumps(users, indent=4, ensure_ascii=False)
+        
     
-
 def main():
-    
-    keys_to_find = ('логин','пароль','пол','возраст')
+    header = ('логин','пароль','пол','возраст')
     
     # filename in another directory?
     FILENAME = '392515-0019-7e.xlsx' # добавить аргумент скрипта | единственный файл 
     workbook = load_workbook(filename=FILENAME, read_only=True, data_only=True)
     sheet = workbook.active
-
-    # сделать так, чтобы получало по списку значений вместо железных значений в функции
-    user_indexes, header_index = get_header_indexes(sheet, keys_to_find) 
-    print(user_indexes)
-    # users_json = create_users_json(sheet, user_indexes)
-    # print(users_json)
-
-
+    users_json = create_users_json(sheet, header)
+    print(users_json)
     # users = json.loads(users_json)
     # print(users['0']['login'])
     # print(users)
